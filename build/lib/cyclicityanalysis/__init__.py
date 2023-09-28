55c3ee31b16e4b96c486c94b2594a90acb4f86af
vikaushik <vskaush2@illinois.edu>
2023-09-28 13:56:46 -0700

    Deleted unnecessary build folder

--- build/lib/cyclicityanalysis/__init__.py
+++
@@ -1,8 +0,0 @@
-# read version from installed package
-from importlib.metadata import version
-__version__ = version("cyclicityanalysis")
-
-# populate package namespace
-from .orientedarea import *
-from .coom import *
-
